869b28c797475c893c0260e6df112d7a295f42a6
Ubuntu <ubuntu@ip-172-31-29-56.eu-north-1.compute.internal>
2026-02-02 11:41:00 +0000

    Add node health check script and ignore editor swap files

---
+++ nodehealth.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+###########################################
+# Author: Blessing Ogbonna
+#
+# Date: 23/01/2026
+#
+# Script: Node Health Check
+#
+# Version: v1
+###########################################
+
+set -x
+
+df -h
+
+free -g
+
+nproc
+
+uptime
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --            
